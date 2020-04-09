a5090e5d49c2838b327fe19985fc9a6bfcdc3e1d
2t0m <2t0m@users.noreply.github.com>
2020-04-09 11:32:52 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-docker stop buzz
-docker rm buzz
-
-docker build -t danobot/buzz .
-docker run --restart=unless-stopped --device=/dev/gpiomem --name buzz -d -e MQTT_HOST="tower.local" -e MQTT_TOPIC="/buzz"  danobot/buzz:latest
